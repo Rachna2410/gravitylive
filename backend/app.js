8bbd5eef23c889ac8bd1c7fbfaa4ec6c06a5b88f
rachna <jainrachna923@gmail.com>
2023-05-04 09:26:19 +0530

    link with frontend

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:3000", // frontend URI (ReactJS)
+  origin: "https://gravitylive-client.onrender.com", // frontend URI (ReactJS)
 };
 
 // db
